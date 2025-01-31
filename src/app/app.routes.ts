64f66a58226f7497be609809f2bb625f123df2fe
JamesTovarR04 <jamestovar09@hotmail.com>
2025-01-30 19:19:56 -0500

    integración msal para autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Route } from '@angular/router';
 import { FullComponent } from '@quarsso/material-pro/layouts/full/full.component';
 import { navItems } from './pages/sidebar-data';
+import { MsalGuard } from '@azure/msal-angular';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     title: 'Accounting',
     component: FullComponent,
+    canActivate: [MsalGuard],
     data: { menu: navItems },
     loadChildren: () =>
       import('./pages/pages.routes').then((m) => m.remoteRoutes),
